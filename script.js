59451943e72bff5ca370e6af08dfda883c8e8ef8
Hadifa Rousheen <hadifarousheen2644@gmail.com>
2025-12-10 19:40:23 +0530

    display time functionality done

---
+++ script.js
@@ -0,0 +1,21 @@
+const hour=document.querySelector('.hour');
+const minute=document.querySelector('.minute');
+const second=document.querySelector('.second')
+const am_pm=document.querySelector('.am-pm')
+
+
+
+setInterval(() => {
+    const hourtime=new Date().getHours();
+const minutetime=new Date().getMinutes();
+const secondtime=new Date().getSeconds();
+
+hour.innerText=hourtime;
+minute.innerText=minutetime;
+second.innerText=secondtime;
+if(hourtime >=12){
+    am_pm.innerText="PM"
+}else{
+    am_pm.innerText="AM"
+}
+}, 1000);
