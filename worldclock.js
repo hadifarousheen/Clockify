75a26ea29b602faf7ff08153b923d3d02924c024
Hadifa Rousheen <hadifarousheen2644@gmail.com>
2025-12-17 15:09:50 +0530

    world clock functionality implemented

--- worldclock.js
+++ worldclock.js
@@ -1,11 +1,50 @@
-const country=document.querySelector('.country');
+
 const getTime=document.querySelector('.getTime');
 const result=document.querySelector('.result')
+const selectOptions=document.querySelector('.selectOptions')
 let userSearch;
-country.addEventListener('change',(e)=>{
-userSearch=e.target.value;
+const baseURL="https://timeapi.io/api/TimeZone/AvailableTimeZones";
+
+let timezones;
+let timezonesJson;
+window.addEventListener("load",async()=>{
+   timezones=await fetch(baseURL);
+    timezonesJson=await timezones.json();
+    console.log(timezonesJson)
+ 
+   
+    for(let i=0;i<timezonesJson.length;i++){
+        const option=document.createElement('option');
+        option.innerText=timezonesJson[i];
+        option.value=timezonesJson[i];
+        selectOptions.addEventListener('change',(e)=>{
+        userSearch=e.target.value;
+        console.log(userSearch)
+        })
+        selectOptions.appendChild(option);
+    }
+   
 })
 
-getTime.addEventListener('click',()=>{
+getTime.addEventListener('click',async()=>{
+const res=await fetch(`https://timeapi.io/api/Time/current/zone?timeZone=${userSearch}`);
+const resJson=await res.json();
+console.log(resJson);
+const time=resJson.time;
+const date=resJson.date;
+const day=resJson.dayOfWeek;
+const timeZone=resJson.timeZone;
+
+const timeTag=document.createElement("p");
+timeTag.innerText=time;
+const dateTag=document.createElement('p');
+dateTag.innerText=date;
+const dayTag=document.createElement('p');
+dayTag.innerText=day;
+const timezoneTag=document.createElement('p');
+timezoneTag.innerText=timeZone;
+
+
+result.append(timezoneTag,dateTag,dayTag,timeTag)
 
 })
