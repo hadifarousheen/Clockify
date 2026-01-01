3066c60260a42ccd6604fc1b5df89a97cd07f897
Hadifa Rousheen <hadifarousheen2644@gmail.com>
2026-01-01 10:52:40 +0530

    home page completed

--- script.js
+++ script.js
@@ -1,21 +1,19 @@
-const hour=document.querySelector('.hour');
-const minute=document.querySelector('.minute');
-const second=document.querySelector('.second')
-const am_pm=document.querySelector('.am-pm')
-
-
+const hour = document.querySelector(".hour");
+const minute = document.querySelector(".minute");
+const second = document.querySelector(".second");
+const am_pm = document.querySelector(".am-pm");
 
 setInterval(() => {
-    const hourtime=new Date().getHours();
-const minutetime=new Date().getMinutes();
-const secondtime=new Date().getSeconds();
+  const hourtime = new Date().getHours();
+  const minutetime = new Date().getMinutes();
+  const secondtime = new Date().getSeconds();
 
-hour.innerText=hourtime;
-minute.innerText=minutetime;
-second.innerText=secondtime;
-if(hourtime >=12){
-    am_pm.innerText="PM"
-}else{
-    am_pm.innerText="AM"
-}
+  hour.innerText = hourtime;
+  minute.innerText = minutetime;
+  second.innerText = secondtime;
+  if (hourtime >= 12) {
+    am_pm.innerText = "PM";
+  } else {
+    am_pm.innerText = "AM";
+  }
 }, 1000);
