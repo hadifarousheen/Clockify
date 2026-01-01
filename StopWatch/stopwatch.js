bbf4cca7da4ccdaf84343fb30d9ee517cb0a3f18
Hadifa Rousheen <hadifarousheen2644@gmail.com>
2026-01-01 11:56:33 +0530

    made stopwatch responsive

--- StopWatch/stopwatch.js
+++ StopWatch/stopwatch.js
@@ -1,42 +1,38 @@
-const start=document.querySelector('.start');
-const stop=document.querySelector('.stop');
-const reset=document.querySelector('.reset');
+const start = document.querySelector(".start");
+const stop = document.querySelector(".stop");
+const reset = document.querySelector(".reset");
 
-const hour=document.querySelector('.hour');
-const minute=document.querySelector('.minute')
-const second=document.querySelector('.second')
-let secondCount=0;
-let minuteCount=0;
-let hourCount=0;
+const hour = document.querySelector(".hour");
+const minute = document.querySelector(".minute");
+const second = document.querySelector(".second");
+let secondCount = 0;
+let minuteCount = 0;
+let hourCount = 0;
 let id;
 
-start.addEventListener('click',()=>{
-   id= setInterval(()=>{
-        if(secondCount>=60){
-            secondCount=0;
-           minute.innerText= ++minuteCount;
+start.addEventListener("click", () => {
+  id = setInterval(() => {
+    if (secondCount >= 60) {
+      secondCount = 0;
+      minute.innerText = ++minuteCount;
+    }
+    if (minuteCount >= 60) {
+      minuteCount = 0;
+      hour.innerText = ++hourCount;
+    }
+    second.innerText = ++secondCount;
+  }, 1000);
+});
 
-        }
-        if(minuteCount >=60){
-            minuteCount=0;
-            hour.innerText=++hourCount;
-        }
-        second.innerText=++secondCount;
-        
-    },1000)
-})
+stop.addEventListener("click", () => {
+  clearInterval(id);
+});
 
-stop.addEventListener('click',()=>{
-
-    clearInterval(id)
-})
-
-reset.addEventListener('click',()=>{
-    secondCount=0;
-    hourCount=0;
-    minuteCount=0;
-            second.innerText="00";
-            minute.innerText="00";
-            hour.innerText="00";
-
-})
+reset.addEventListener("click", () => {
+  secondCount = 0;
+  hourCount = 0;
+  minuteCount = 0;
+  second.innerText = "00";
+  minute.innerText = "00";
+  hour.innerText = "00";
+});
